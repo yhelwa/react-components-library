4af783d50a50bcf46cc0449846805aa8e4cac02f
Youssef Helwa <67173550+yhelwa@users.noreply.github.com>
2025-01-06 13:46:04 +0300

    Updated button

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,49 +1,41 @@
 // rollup.config.js
-import terser from '@rollup/plugin-terser';
+import terser from "@rollup/plugin-terser";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-
-
-import pkg from "./package.json" assert { type: 'json' };
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import pkg from "./package.json" assert { type: "json" };
 
 export default [
-	{
-		input: 'src/index.ts',
-		output: [
-			{
-				file: pkg.main,
-				format: 'cjs',
-				sourcemap: true,
-			},
-			{
-				file: pkg.module,
-				format: "esm",
-				sourcemap: true,
-			},
-			/* {
-				file: 'dist/bundle.min.js',
-				format: 'iife',
-				name: 'version',
-				plugins: [terser()]
-			} */
-		],
-		plugins: [
-			peerDepsExternal(),
-			resolve(),
-			commonjs(),
-			typescript({ tsconfig: "./tsconfig.json" }),
-			postcss(),
-			terser()
-		]
-	},
-	{
-		input: "dist/esm/types/index.d.ts",
-		output: [{ file: "dist/index.d.ts", format: "esm" }],
-		plugins: [dts()],
-		external: [/\.(css|less|scss)$/],
-	},
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+      terser(),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
+  },
 ];
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Button.scss";
 
-export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>{
+export interface ButtonProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
   label: string;
+  onClick?: () => void;
 }
 
-const Button = ({label, ...others}: ButtonProps) => {
+const Button = ({ label, ...others }: ButtonProps) => {
   return <button {...others}>{label}</button>;
 };
 
