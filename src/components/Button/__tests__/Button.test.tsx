83dc63d66d417e0e09685db15fdf3e2f7bab92b6
Youssef Helwa <67173550+yhelwa@users.noreply.github.com>
2025-01-07 12:32:33 +0300

    Added sequential job

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Button from "../Button";
 
-
-
 describe("Button", () => {
   test("renders the Button component", () => {
-    render(<Button label="Hello world!" />);
+    const renderButton = render(<Button label="Hello world!" />);
+    //create a failure
+    expect(renderButton.getByText("Hello world!")).not.toBeCalled();
   });
 });
